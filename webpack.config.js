159c115e1e52ede170431c7d682b990577c13a39
Gabriel <gabicardetti@gmail.com>
2019-05-24 17:24:38 -0300

    agrego variables de entorno en Api clima

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -32,7 +33,8 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new Dotenv()
   ],
   devtool: 'inline-source-map',
   devServer: {
