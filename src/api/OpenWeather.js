dfeb385cf046aa79cb40949c3f9b532fbf74dc18
GastonAznarez <gastonaznarez@gmail.com>
2019-05-28 11:02:44 -0300

    Cambios en apis

--- src/api/OpenWeather.js
+++ src/api/OpenWeather.js
@@ -1,22 +1,19 @@
-
-class OpenWather {
-  GETWeather (city) {
-    return new Promise( (resolve,reject) => {
-      const key = process.env.OPENWEATHER_API_KEY
-      if(city === "") return
-      const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + ',AR&appid=' + key
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          resolve(data)
-        })
-        .catch(error => {
-          console.error(error)
-          reject(error)
-        })
+const getWeather = (lat, lon) => {
+  return new Promise( (resolve,reject) => {
+    const key = 'f56c89511abe2b1ee423e47a55199c41';
+    const unit = 'metric';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=lat${lat}&lon${lon}&unit=${unit}&appid=${key}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        resolve(data)
       })
-   }
+      .catch(error => {
+        console.error(error)
+        reject(error)
+      });
+    });
  }
 
- export default new OpenWather()
+ export { getWeather }
