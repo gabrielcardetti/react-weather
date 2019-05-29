bbea02013a9d11ebbc239a8dfed623bf990e295e
Gabriel <gabicardetti@gmail.com>
2019-05-29 12:57:45 -0300

    Cambio estilo header

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,9 +6,11 @@ import { Header, Grid, Icon } from 'semantic-ui-react'
 const details = props => {
     return(
       <div>
+        <div id='headerDetail'>
+          <Header size='huge'>{props.day}</Header>
+        </div>
         <div id='datailsContainer'>
           <div id='leftDetail'>
-            <Header size='huge'>{props.day}</Header>
             <p>??: {props.numberday}</p>
             <p>Humedad: {props.humidity}</p>
             <p>Presion: {props.pressure}</p>
