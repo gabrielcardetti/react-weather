ae13f171be87e57f1b852b76eaa53bf45c3712d5
Gabriel <gabicardetti@gmail.com>
2019-06-07 11:45:56 -0300

    eslint correciones

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
@@ -8,48 +8,48 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        use:['style-loader','css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(?:png|jpg|svg)$/,
         loader: 'url-loader',
         query: {
           // Inline images smaller than 10kb as data URIs
-          limit: 10000
-        }
+          limit: 10000,
+        },
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [{
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
-                outputPath: 'fonts/'
-            }
-        }]
-      }
-    ]
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
+        }],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: __dirname + '/public',
+    path: `${__dirname}/public`,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new Dotenv()
+    new Dotenv(),
   ],
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: __dirname + '/public',
+    contentBase: `${__dirname}/public`,
     inline: true,
-    hot: true
-  }
+    hot: true,
+  },
 };
